8da7db9fa2ea9f4ffd40eff0ddd6da0633f3bfc0
Justin Lawson <jstn.lawson@gmail.com>
2023-06-12 22:17:43 -0500

    finished required

--- script.js
+++ script.js
@@ -18,7 +18,10 @@ function onReady() {
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
     
-    $('#arcane-scepter').click (arcaneAttack)
+    $('.arcane-scepter').click (arcaneAttack)
+    $('.entangle').click (entangleAttack)
+    $('.dragon-blade').click(dragonAttack)
+    $('.star-fire').click(starFireAttack)
 
     // 🧠 Remember
     // - Handle events that ->
@@ -34,18 +37,127 @@ function onReady() {
 function arcaneAttack() {
     console.log('in arcane attack')
     //take the fungus hp and player ap down by 10
-    fungusHP = Number($("#fungus-points").text())
-    let arcaneDamage = Number(fungusHP) - 14
-        if (arcaneDamage < 0) {
-                arcaneDamage = 0
-            }
     playerAP = Number($('#attack-points').text())
-    let arcaneCost = Number(playerAP) - 12
-            if (arcaneCost < 1) {
-                arcaneCost = 0
-            }
-    $('#fungus-points').text(arcaneDamage)
-    $('#attack-points').text(arcaneCost)    
+    let weaponCost = Number(playerAP) - 12
+    if (weaponCost < 0) {
+        weaponCost = 0
+        $(this).prop('disabled', true)
+        deathOfPlayer()
+    }
+    fungusHP = Number($("#fungus-points").text())
+    let weaponDamage = Number(fungusHP) - 14
+    if (weaponDamage < 0) {
+        weaponDamage = 0
+        deathOfFungus()
+    }
+    $('#fungus-points').text(weaponDamage)
+    $('#attack-points').text(weaponCost)
+}
+
+// Entangle
+
+// - **AP Cost:** 23
+// - **HP Damage:** 9
+
+
+function entangleAttack() {
+    playerAP = Number($('#attack-points').text())
+    let weaponCost = Number(playerAP) - 23
+    if (weaponCost < 0) {
+        weaponCost = 0
+        $(this).prop('disabled', true)
+        deathOfPlayer()
+    }
+    fungusHP = Number($("#fungus-points").text())
+    let weaponDamage = Number(fungusHP) - 9
+    if (weaponDamage < 0) {
+        weaponDamage = 0
+        deathOfFungus()
+    }
+    $('#fungus-points').text(weaponDamage)
+    $('#attack-points').text(weaponCost)
+}
+
+// Dragon Blade
+
+// - **AP Cost:** 38
+// - **HP Damage:** 47
+
+function dragonAttack(){
+    playerAP = Number($('#attack-points').text())
+    let weaponCost = Number(playerAP) - 38
+    if (weaponCost < 0) {
+        weaponCost = 0
+        $(this).prop('disabled', true)
+        deathOfPlayer()
+    }
+    fungusHP = Number($("#fungus-points").text())
+    let weaponDamage = Number(fungusHP) - 47
+    if (weaponDamage < 0) {
+        weaponDamage = 0
+        deathOfFungus()
+    }
+    $('#fungus-points').text(weaponDamage)
+    $('#attack-points').text(weaponCost)
+}
+
+// Star Fire
+
+// - **AP Cost:** 33
+// - **HP Damage:** 25
+
+function starFireAttack() {
+    playerAP = Number($('#attack-points').text())
+    let weaponCost = Number(playerAP) - 33
+    if (weaponCost < 0) {
+        weaponCost = 0
+        $(this).prop('disabled', true)
+        deathOfPlayer()
+    }
+    fungusHP = Number($("#fungus-points").text())
+    let weaponDamage = Number(fungusHP) - 25
+    if (weaponDamage < 0) {
+        weaponDamage = 0
+        deathOfFungus()
+    }
+    $('#fungus-points').text(weaponDamage)
+    $('#attack-points').text(weaponCost)
 }
 
 
+// - If the Freaky Fungus runs out of HP, the monster is 
+// dead and you win! Replace the `walk` class with a `dead` 
+// class on the freaky-fungus element, to make them fall 
+// over and die.
+
+function deathOfFungus() {
+        $('#fungus-animation').removeClass('walk') 
+        $('#fungus-animation').addClass('hurt')
+}
+
+// - If you run out of AP, the monster wins and humanity is 
+// doomed 😢 Replace the `walk` class with a `jump` class on 
+// the freaky-fungus element, to make them jump for the glory 
+// of the fungal race.
+
+function deathOfPlayer() {
+    $('#fungus-animation').removeClass('walk')
+    $('#fungus-animation').addClass('jump')
+}
+
+// function disableAttack() {
+//     $(this).prop('disabled', true)
+// }
+
+// function render() {
+//     if (weaponDamage < 0) {
+//         weaponDamage = 0
+//         deathOfFungus()
+//     }
+//     if (weaponCost < 1) {
+//         weaponCost = 0
+//         deathOfPlayer()
+//     }
+// // $('#fungus-points').text(weaponDamage)
+// // $('#attack-points').text(weaponCost)
+// }
